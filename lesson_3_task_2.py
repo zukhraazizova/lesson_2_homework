78344eb91facf6c6362c0721d321594279b20338
zukhraazizova <zuhralipa@gmail.comclear>
2025-09-26 00:00:29 +0300

    Добавлены файлы для lesson_3 (Address, Mailing, задачи)

--- lesson_3_task_2.py
+++ lesson_3_task_2.py
@@ -4,7 +4,9 @@ from smartphone import Smartphone
 catalog =[
     Smartphone("samsung","s25","+712345678"),
     Smartphone("iphone","17pro","+78906543"),
-    Smartphone("xioami","13","+789065432")
+    Smartphone("xioami","13","+789065432"),
+    Smartphone("poco","f7","+78564542"),
+    Smartphone("honor","13i","+73563562")
 ]
 
 for smartphone in catalog: 
